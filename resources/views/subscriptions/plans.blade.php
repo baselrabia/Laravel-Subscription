58ccf5d022f77a45b83a0ab0805e81da2912f36b
baselrabia <baselrabia@gmail.com>
2020-08-19 00:52:10 +0200

    3-creating-and-listing-plans

---
+++ resources/views/subscriptions/plans.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Plans') }}</div>
+
+                <div class="card-body">
+                    @foreach ($plans as $plan)
+                        <div>
+                                <a href="#" >  {{$plan->title}} </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Subscriptions'], function(){
+    Route::get('plans', 'PlanController@index')->name('plans');
+
+});
