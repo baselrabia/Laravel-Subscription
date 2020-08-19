27017c8f9a52bae451aec99a9425ac39eb9c9193
baselrabia <baselrabia@gmail.com>
2020-08-19 02:13:58 +0200

    4-setting-up-laravel-cashier

--- app/User.php
+++ app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
+    use Billable;
     use Notifiable;
 
     /**
